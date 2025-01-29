83ce4e4107286b32d0a6467b93f66bca4fe5e24d
daiki <daikimorita030612@gmail.com>
2025-01-29 15:50:21 +0900

    マイページ

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <header>
 <a href= "https://app.slack.com/client/T034EJWA9M5/C05H8SALD2N" target="_blank" >別ウインドウで開くリンク</a>
 </header>
-<h1>emobi</h1>
+<h1>マイページ</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <footer>
 <img

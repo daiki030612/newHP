59d118874bc1cdf7011d0e7736e09124986dcec9
daiki <daikimorita030612@gmail.com>
2025-01-29 14:53:08 +0900

    最初のコミット

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<header>
+<a href= "https://app.slack.com/client/T034EJWA9M5/C05H8SALD2N" target="_blank" >別ウインドウで開くリンク</a>
+</header>
+<h1>emobi</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<footer>
+<img
+  src="IMG_6978.jpg"
+  alt="Grapefruit slice atop a pile of other slices" />
+</footer>
